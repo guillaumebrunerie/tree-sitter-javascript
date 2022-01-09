460c9a7d11b09d23a1acb1c71ed95dfe5a9facac
Guillaume Brunerie <guillaume.brunerie@gmail.com>
2022-01-09 21:01:58 +0100

    Add missing highlighting rules for parameters.

--- queries/highlights-params.scm
+++ queries/highlights-params.scm
@@ -7,6 +7,11 @@
       [
         (pair_pattern value: (identifier) @variable.parameter)
         (shorthand_property_identifier_pattern) @variable.parameter
+        (rest_pattern (identifier) @variable.parameter)
       ])
+     (assignment_pattern
+       (identifier) @variable.parameter)
   ]
 )
+
+(arrow_function . (identifier) @variable.parameter)
